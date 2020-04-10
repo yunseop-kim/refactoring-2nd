906567d87ac1adbb39be4de188f18ed653f8ea8f
Yunseop Kim <undefined@mealant.com>
2020-04-10 22:25:55 +0900

    1.7 중간점검 (두 파일로 분리됨) 61-61p 보이스카웃 원칙

--- index.js
+++ index.js
@@ -18,4 +18,22 @@ function renderPlainText(data) {
   return result;
 }
 
+function htmlStatement(invoice, plays) {
+  return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data) {
+  let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>\n`;
+  result += "<table>\n";
+  result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>";
+  for (let perf of data.performances) {
+    result += `<tr><td>${perf.play.name}</td><td>(${perf.audience}석)</td>`;
+    result += `<td>${usd(perf.amount)} </td></tr>\n`;
+  }
+  result += "</table>\n";
+  result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>적립 포인트: <em>${data.totalVolumeCredits}점</em></p>\n`;
+  return result;
+}
+
 console.log(statement(invoices, play));
